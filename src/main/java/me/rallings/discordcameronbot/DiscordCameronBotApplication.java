f7d8c020d4350cad980d5170380265769ab5bccf
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-07-05 13:48:06 -0400

    Initial commit

---
+++ src/main/java/me/rallings/discordcameronbot/DiscordCameronBotApplication.java
@@ -0,0 +1,13 @@
+package me.rallings.discordcameronbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordCameronBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordCameronBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/rallings/discordcameronbot/DiscordCameronBotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rallings.discordcameronbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordCameronBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
